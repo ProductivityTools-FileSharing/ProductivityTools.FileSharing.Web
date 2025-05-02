40de983e40ef6b467fe7a0b47bd427a6616d4579
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-02 09:17:58 +0200

    config

--- src/Config.js
+++ src/Config.js
@@ -5,7 +5,7 @@ const dev = {
 
 
 const prod = {
-    serverUrl : 'http://kameralne.productivitytools.top:8004'
+    serverUrl : 'http://kameralne.productivitytools.top:8005'
 }
 export const config = process.env.NODE_ENV === 'development' ? prod : prod;
 //export const config = process.env.NODE_ENV === 'development' ? dev : prod;
