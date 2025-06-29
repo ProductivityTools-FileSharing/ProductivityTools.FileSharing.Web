0f0d5de0bd4fc773bd6592174f23884c8b8a4773
Pawel Wujczyk <pwujczyk@gmail.com>
2025-06-29 19:15:24 +0200

    dev remove

--- src/Config.js
+++ src/Config.js
@@ -9,5 +9,5 @@ const prod = {
     serverUrl : 'https://file.productivitytools.top:8005',
     fileUrl: 'https://filesharinggs.blob.core.windows.net/filecontainergs'
 }
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export const config = process.env.NODE_ENV === 'development' ? prod : prod;
 //export const config = process.env.NODE_ENV === 'development' ? dev : prod;
